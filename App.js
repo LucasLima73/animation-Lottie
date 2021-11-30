348665beb8aa2647da0d60ffe3f724063c4c1848
Lucas <lucaslavey@outlook.com>
2021-11-30 16:19:44 -0300

    refector app and add Moti

--- App.js
+++ App.js
@@ -2,40 +2,59 @@ import { StatusBar } from "expo-status-bar";
 import React, { useRef, useEffect, useState } from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import LottieView from "lottie-react-native";
+import { MotiView, MotiText } from "moti";
 
 export default function App() {
-  const [save, setSave] = useState(true);
   const animation = useRef(null);
-  const firstRun = useRef(true);
-
-  useEffect(() => {
-    if (firstRun.current) {
-      if (save) {
-        animation.current.play(94, 94);
-      } else {
-        animation.current.play(7, 7);
-      }
-
-      firstRun.current = false;
-    } else if (save) {
-      animation.current.play(0, 250);
-    } else {
-      animation.current.play(0, 0);
-    }
-  }, [save]);
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={() => setSave(!save)}>
-        <LottieView
-          source={require("./src/assets/sun.json")}
-          style={{ width: 300, height: 300 }}
-          autoPlay={false}
-          resizeMode="cover"
-          ref={animation}
-        />
-      </TouchableOpacity>
-    </View>
+    <MotiView
+      from={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ type: "timing", duration: 5000 }}
+      style={styles.container}
+    >
+      <MotiText
+        from={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ type: "timing", duration: 5000, delay: 2000 }}
+        style={{ fontSize: 40, marginBottom: 20, fontWeight: "bold" }}
+      >
+        Lucas Lima
+      </MotiText>
+      <LottieView
+        source={require("./src/assets/walking.json")}
+        style={{ width: 300, height: 300 }}
+        resizeMode="cover"
+        ref={animation}
+        autoPlay={true}
+      />
+      {/* <TouchableOpacity
+        style={{
+          width: "90%",
+          height: 100,
+          marginTop: 20,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        onPress={() => {
+          animation.current.play();
+          setTimeout(() => {
+            animation.current.reset();
+          }, 10000);
+        }}
+      >
+        <MotiText
+          from={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ type: "timing", duration: 5000, delay: 2000 }}
+          style={{ fontSize: 40, marginBottom: 20, fontWeight: "bold" }}
+          style={{ fontSize: 32 }}
+        >
+          Load
+        </MotiText>
+      </TouchableOpacity> */}
+    </MotiView>
   );
 }
 
